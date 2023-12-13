9f088f2dbd08f6a35b589275efc65c44f4865357
Dima <jimbin23@gmail.com>
2023-12-13 19:52:03 +0300

    About

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello Dima!')
-})
-
 app.get('/about', (req, res) => {
     res.send('Hello Dima, I will tell you about our company!')
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello Dima!')
 })
 
 app.listen(port, () => {
