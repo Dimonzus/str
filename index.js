5f93167dd1cc703836276fca2737161bed5e7486
Dima <jimbin23@gmail.com>
2023-12-13 23:11:59 +0300

    add an route

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ app.get('/about', (req, res) => {
     res.send('Hello Dima, I will tell you about our company!')
 })
 
+app.get('/prod', (req, res) => {
+    res.send('Here aour products')
+})
+
 app.get('/', (req, res) => {
     res.send('Hello Dima!')
 })
 
-
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-}) 
+})
