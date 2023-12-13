505fede998ed0a084babb0d59bc6ff98547d7215
Dima <jimbin23@gmail.com>
2023-12-13 19:47:15 +0300

    Added about

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('Hello Dima!')
 })
 
+app.get('/about', (req, res) => {
+    res.send('Hello Dima, I will tell you about our company!')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
